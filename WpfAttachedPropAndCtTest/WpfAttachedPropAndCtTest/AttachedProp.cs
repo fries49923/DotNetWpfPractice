c1ca6934618fe54e4fdceb3e585c8b0b5ab7c399
ポテト <fries49923@gmail.com>
2025-04-16 00:39:22 +0800

    Add and Init WpfAttachedPropAndCtTest
    1. 增加WpfAttachedPropAndCtTest解決方案
    2. 初步撰寫WPF附加屬性與ControlTemplate簡單使用範例

---
+++ WpfAttachedPropAndCtTest/WpfAttachedPropAndCtTest/AttachedProp.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfAttachedPropAndCtTest
+{
+    public static class AttachedProp
+    {
+        public static readonly DependencyProperty IconFillProperty =
+            DependencyProperty.RegisterAttached(
+                "IconFill",
+                typeof(Brush),
+                typeof(AttachedProp),
+                new FrameworkPropertyMetadata(Brushes.Black, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetIconFill(UIElement element, Brush value)
+        {
+            element.SetValue(IconFillProperty, value);
+        }
+
+        public static Brush GetIconFill(UIElement element)
+        {
+            return (Brush)element.GetValue(IconFillProperty);
+        }
+    }
+}
---
+++ WpfAttachedPropAndCtTest/WpfAttachedPropAndCtTest/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfAttachedPropAndCtTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
