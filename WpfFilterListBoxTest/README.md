729db59427ca4079150023768fb8dfb29ddf6a46
ポテト <fries49923@gmail.com>
2025-04-15 23:20:22 +0800

    Add WpfFilterListBoxTest
    1. Add WpfFilterListBoxTest

---
+++ WpfFilterListBoxTest/README.md
@@ -0,0 +1,3 @@
+#ListBox #CollectionView #Filter
+
+示範使用CollectionView，輸入字串到TextBox中，讓ListBox只顯示濾除後的結果
---
+++ WpfFilterListBoxTest/WpfFilterListBoxTest/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace WpfFilterListBoxTest
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private CollectionView view;
+
+        [ObservableProperty]
+        private string _filterText;
+
+        public ObservableCollection<string> TestStrings
+        { get; private set; }
+
+        public MainViewModel()
+        {
+            _filterText = "";
+
+            TestStrings = new()
+            {
+                "Pizza",
+                "Sushi",
+                "Burger",
+                "Pasta",
+                "Fried Rice",
+                "Ramen",
+                "Tacos",
+                "Salad",
+                "Steak",
+                "Dumplings"
+            };
+
+            view = (CollectionView)CollectionViewSource.GetDefaultView(TestStrings);
+            view.Filter = CustomFilter;
+        }
+
+        private bool CustomFilter(object obj)
+        {
+            if (string.IsNullOrWhiteSpace(FilterText) is true)
+                return true;
+
+            if (obj is string item)
+                return item.Contains(FilterText, StringComparison.OrdinalIgnoreCase);
+
+            return false;
+        }
+
+        [RelayCommand]
+        private void SearchText()
+        {
+            CollectionViewSource.GetDefaultView(TestStrings).Refresh();
+        }
+    }
+}
