281e20bef2569fbff2f37711b2ba7342ef428acd
ポテト <fries49923@gmail.com>
2025-05-01 11:11:50 +0800

    Add WpfListBoxDataTemplateDataTypeTest
    1. Add WpfListBoxDataTemplateDataTypeTest

---
+++ WpfListBoxDataTemplateDataTypeTest/WpfListBoxDataTemplateDataTypeTest/AirplaneControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfListBoxDataTemplateDataTypeTest
+{
+    public class AirplaneControl : Control
+    {
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(AirplaneControl));
+
+        public static readonly DependencyProperty ThrustProperty =
+            DependencyProperty.Register("Thrust", typeof(string), typeof(AirplaneControl));
+
+        public static readonly DependencyProperty WingspantProperty =
+            DependencyProperty.Register("Wingspant", typeof(string), typeof(AirplaneControl));
+
+        public static readonly DependencyProperty EngineCountProperty =
+            DependencyProperty.Register("EngineCount", typeof(string), typeof(AirplaneControl));
+
+        static AirplaneControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(AirplaneControl), new FrameworkPropertyMetadata(typeof(AirplaneControl)));
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string Thrust
+        {
+            get => (string)GetValue(ThrustProperty);
+            set => SetValue(ThrustProperty, value);
+        }
+
+        public string Wingspant
+        {
+            get => (string)GetValue(WingspantProperty);
+            set => SetValue(WingspantProperty, value);
+        }
+
+        public string EngineCount
+        {
+            get => (string)GetValue(EngineCountProperty);
+            set => SetValue(EngineCountProperty, value);
+        }
+    }
+}
---
+++ WpfListBoxDataTemplateDataTypeTest/WpfListBoxDataTemplateDataTypeTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using WpfListBoxDataTemplateDataTypeTest.Model;
+
+namespace WpfListBoxDataTemplateDataTypeTest.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<VehicleBase> Vehicles
+        { get; protected set; }
+
+        public MainViewModel()
+        {
+            Vehicles = [];
+        }
+
+        [RelayCommand]
+        protected void WindowLoad()
+        {
+            Vehicles.Add(
+                new Car
+                {
+                    Name = "SportCar X1",
+                    Horsepower = 320,
+                    Weight = 1350.5
+                });
+
+            Vehicles.Add(
+                new Boat
+                {
+                    Name = "SeaRider 200",
+                    EnginePower = 500,
+                    Length = 12.3
+                });
+
+            Vehicles.Add(
+                new Boat
+                {
+                    Name = "WaveSurfer",
+                    EnginePower = 320,
+                    Length = 9.7
+                });
+
+            Vehicles.Add(
+                new Airplane
+                {
+                    Name = "SkyJet A5",
+                    Thrust = 180.0,
+                    Wingspan = 27.8,
+                    EngineCount = 2
+                });
+
+            Vehicles.Add(
+                new Car
+                {
+                    Name = "EcoCar Z",
+                    Horsepower = 110,
+                    Weight = 980.0
+                });
+        }
+    }
+}
