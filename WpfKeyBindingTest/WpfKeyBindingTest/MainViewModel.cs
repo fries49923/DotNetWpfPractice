627b005751f24c75a06cb555142b6fc0626e231e
ポテト <fries49923@gmail.com>
2025-05-06 23:51:28 +0800

    Add WpfKeyBindingTest
    1. Add WpfKeyBindingTest

---
+++ WpfKeyBindingTest/WpfKeyBindingTest/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+
+namespace WpfKeyBindingTest
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        protected Visibility _btnVisibility;
+
+        public MainViewModel()
+        {
+            _btnVisibility = Visibility.Visible;
+        }
+
+        [RelayCommand]
+        protected void HotKeyTest()
+        {
+            BtnVisibility =
+                BtnVisibility is Visibility.Collapsed
+                ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
