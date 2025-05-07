b2e64e21676255fd3ef125a0f4b31ef74418f6e1
ポテト <fries49923@gmail.com>
2025-05-08 00:27:02 +0800

    Add WpfListBoxAlternationIndexTest
    1. Add WpfListBoxAlternationIndexTest

---
+++ WpfListBoxAlternationIndexTest/README.md
@@ -0,0 +1,3 @@
+#ListBox #AlternationIndex #AlternationCount
+
+測試ListBox可以經過設定，來讓項目根據索引的值來使用不同的設定，例如背景顏色
---
+++ WpfListBoxAlternationIndexTest/WpfListBoxAlternationIndexTest/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WpfListBoxAlternationIndexTest
+{
+    public class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<string> Items
+        { get; set; }
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5",
+                "Item 6",
+                "Item 7",
+                "Item 8",
+                "Item 9",
+                "Item 10",
+            };
+        }
+    }
+}
