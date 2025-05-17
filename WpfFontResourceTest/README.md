b0e61cc0f949127608f0af48f0498d01a57cbf22
ポテト <fries49923@gmail.com>
2025-05-17 15:19:21 +0800

    Add WpfFontResourceTest
    1. WpfFontResourceTest

---
+++ WpfFontResourceTest/README.md
@@ -0,0 +1,3 @@
+#FontResource #FontFamily #ttf
+
+測試在 WPF 中使用 TTF 字體檔案，並透過 Resource 字典來管理和使用字體
---
+++ WpfFontResourceTest/WpfFontResourceTest/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WpfFontResourceTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //string projectRoot = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            //string fontPath = Path.Combine(projectRoot, "Fonts", "Kalam", "Kalam-Regular.ttf");
+
+            //var pfc = new PrivateFontCollection();
+            //pfc.AddFontFile(fontPath);
+            //Debug.WriteLine($"字體名稱: {pfc.Families[0].Name}");
+        }
+    }
+}
